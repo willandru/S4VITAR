c30888549512e2fff207ff67ed944af70288f730
willandru <willandru@hotmail.com>
2021-11-27 23:55:31 -0500

    ...

--- first.php
+++ first.php
@@ -1,3 +1,15 @@
 <?php
-	system('whoami');
+	system('whoami');  //Esto es un comentario y se supone qu e no lo puedes ver 
 ?>
+
+
+
+
+
+
+
+//  localhost/first.php?file=php://filter/convert_base64-encode/resorce=first.php
+
+			//# echo "localhost/first.php?file=php://filter/convert_base64-encode/resorce=first.php" | base64 -d; echo
+
+			
